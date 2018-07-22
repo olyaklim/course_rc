ed68c7c005c05fecf70723c0ae12bb4da0e38c58
Olga Klimenko <olya_klim@ukr.net>
2018-07-23 02:38:31 +0300

    slide

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,43 @@
 import sayHello from './lib/sayHello.js';
+import 'slick-carousel';
 
 sayHello();
 
+$('.js-news-ban').slick({
+  dotsClass: 'news-ban__dots',
+  arrows: false,
+  dots: true,
+  fade: true,
+  adaptiveHeight: true,
+  autoplay: true,
+  speed: 500
+});
+
 
 $(document).ready(function() {
-
-  // mob-menu
-  if ($(window).width() < 1024) {
-    $('.js-parent .main-menu__link').removeAttr('href');
-
-    $('.js-parent .main-menu__link').click(function() {
-      $(this).closest('.js-parent').find('.main-menu__submenu').slideToggle(300);
-    });
-  }
 
   // modal +
   $('[data-modal]').click(function() {
     var thisId = $(this).attr('data-modal');
     $('body').toggleClass('fixed');
-    $('[data-id=' + thisId +']').toggleClass('active');
+    $('[data-id=' + thisId +']').toggleClass('is-active');
 
     setTimeout(function() {
-      $('[data-id=' + thisId +']').toggleClass('open');
+      $('[data-id=' + thisId +']').toggleClass('is-open');
+      $('.header-page__mobil-close').toggleClass('is-active');
     }, 50);
   });
 
   $('[data-close]').click(function() {
     setTimeout(function() {
       $('body').removeClass('fixed');
-      $('[data-id]').removeClass('active');
+      $('[data-id]').removeClass('is-active');
+
     }, 500);
 
-    $('[data-id]').removeClass('open');
+    $('[data-id]').removeClass('is-open');
+    $('.header-page__mobil-close').removeClass('is-active');
   });
   // modal -
 
 });
-
-
-
